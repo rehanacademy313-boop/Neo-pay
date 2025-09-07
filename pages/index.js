4b622717fc72537e22905106722b961a8c1b73ae
rehanacademy313-boop <rehanacademy313@gmail.com>
2025-09-07 05:34:05 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,6 @@
 import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className="text-center mt-10">
-      <h1 className="text-3xl font-bold">Welcome to NeoPay</h1>
-      <Link href="/register">
-        <button className="mt-6 bg-blue-500 text-white px-4 py-2 rounded">
-          Register Now
-        </button>
-      </Link>
-    </div>
-  );
-}
+<Link href="/register">
+  <a className="mt-6 inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
+    Register Now
+  </a>
+</Link>
